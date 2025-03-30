a18db7372e953d7bad4723ab13fc3c2c310cb436
Ayush Bansal <pk3535745@gmail.com>
2025-03-30 15:50:59 +0530

    Updated for generalization

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-var radius = 500; // how big of the radius
+var radius = 200; // how big of the radius
 var autoRotate = true; // auto rotate or not
 var rotateSpeed = -60; // unit: seconds/360 degrees
-var imgWidth = 120; // width of images (unit: px)
-var imgHeight = 170; // height of images (unit: px)
+var imgWidth = 150; // width of images (unit: px)
+var imgHeight = 200; // height of images (unit: px)
 
 // Link of background music - set 'null' if you dont want to play background music
-var bgMusicURL = 'imgs/Drama Queen.mp3';
+var bgMusicURL = 'imgs/Radha Krishna Title Song.mp3';
 var bgMusicControls = true; // Show UI music control
 
 
@@ -65,7 +65,7 @@ if (autoRotate) {
 // add background music
 if (bgMusicURL) {
   document.getElementById('music-container').innerHTML += `
-<audio src="imgs/Drama Queen.mp3" ${bgMusicControls? 'controls': ''} autoplay loop>    
+<audio src="imgs/Radha Krishna Title Song.mp3" ${bgMusicControls? 'controls': ''} autoplay loop>    
 <p>If you are reading this, it is because your browser does not support the audio element.</p>
 </audio>
 `;
