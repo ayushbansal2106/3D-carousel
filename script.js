b31a9affd36f3815b509a47e9e69c0fdce914171
Ayush Bansal <pk3535745@gmail.com>
2025-03-30 14:17:37 +0530

    song updation

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var imgWidth = 120; // width of images (unit: px)
 var imgHeight = 170; // height of images (unit: px)
 
 // Link of background music - set 'null' if you dont want to play background music
-var bgMusicURL = 'https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a';
+var bgMusicURL = 'imgs\Drama Queen.mp3';
 var bgMusicControls = true; // Show UI music control
 
 
@@ -65,7 +65,7 @@ if (autoRotate) {
 // add background music
 if (bgMusicURL) {
   document.getElementById('music-container').innerHTML += `
-<audio src="${bgMusicURL}" ${bgMusicControls? 'controls': ''} autoplay loop>    
+<audio src="imgs\Drama Queen.mp3" ${bgMusicControls? 'controls': ''} autoplay loop>    
 <p>If you are reading this, it is because your browser does not support the audio element.</p>
 </audio>
 `;
