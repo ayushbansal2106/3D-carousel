3ae012b9fba11321fe8ee8bbd815ab3345604a2f
Ayush Bansal <pk3535745@gmail.com>
2025-03-30 14:24:34 +0530

    song update

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var imgWidth = 120; // width of images (unit: px)
 var imgHeight = 170; // height of images (unit: px)
 
 // Link of background music - set 'null' if you dont want to play background music
-var bgMusicURL = 'imgs\Drama Queen.mp3';
+var bgMusicURL = 'imgs/Drama Queen.mp3';
 var bgMusicControls = true; // Show UI music control
 
 
@@ -65,7 +65,7 @@ if (autoRotate) {
 // add background music
 if (bgMusicURL) {
   document.getElementById('music-container').innerHTML += `
-<audio src="imgs\Drama Queen.mp3" ${bgMusicControls? 'controls': ''} autoplay loop>    
+<audio src="imgs/Drama Queen.mp3" ${bgMusicControls? 'controls': ''} autoplay loop>    
 <p>If you are reading this, it is because your browser does not support the audio element.</p>
 </audio>
 `;
